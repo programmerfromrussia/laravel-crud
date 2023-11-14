602b772af5afcac78a3861441134ed21afcb298a
programmerfromrussia <112801852+programmerfromrussia@users.noreply.github.com>
2023-11-14 15:08:23 +0300

    My last commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,71 @@
-<h1>hello</h1>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+  @auth
+  <p>Вы успешно вошли в свой аккаунт!</p>
+  <form action="/logout" method="POST">
+    @csrf
+    <button>Выйти</button>
+  </form>
+
+  <div style="border: 3px solid black;">
+    <h2>Создать новый пост</h2>
+    <form action="/create-post" method="POST">
+      @csrf
+      <input type="text" name="title" placeholder="название поста">
+      <textarea name="body" placeholder="содержание поста..."></textarea>
+      <button>Сохранить</button>
+    </form>
+  </div>
+
+  <div style="border: 3px solid black;">
+    <h2>Все посты</h2>
+    @foreach($posts as $post)
+    <div style="background-color: gray; padding: 10px; margin: 10px;">
+
+      <h3>{{$post['title']}} by {{$post->user->name}}</h3>
+      {{$post['body']}}
+
+      <p><a href="/edit-post/{{$post->id}}">Редактировать</a></p>
+
+      <form action="/delete-post/{{$post->id}}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button>Удалить</button>
+      </form>
+
+    </div>
+    @endforeach
+  </div>
+
+  @else
+  <div style="border: 3px solid black;">
+    <h2>Зарегистрируйте ваш аккаунт</h2>
+    <form action="/register" method="POST">
+      @csrf
+      <input name="name" type="text" placeholder="имя">
+      <input name="email" type="text" placeholder="элекронная почта">
+      <input name="password" type="password" placeholder="пароль">
+      <button>Зарегистрироваться</button>
+    </form>
+  </div>
+  <div style="border: 3px solid black;">
+    <h2>Войдите в ваш аккаунт</h2>
+    <form action="/login" method="POST">
+      @csrf
+      <input name="loginname" type="text" placeholder="имя">
+      <input name="loginpassword" type="password" placeholder="пароль">
+      <button>Войти</button>
+    </form>
+  </div>
+  @endauth
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
+use \Illuminate\Support\Facades\Route;
+use App\Models\Post;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,16 +10,25 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('home');
+    $posts = [];
+    if (auth()->check()) {
+        $posts = auth()->user()->usersCoolPosts()->latest()->get();
+    }
+
+    return view('home', ['posts' => $posts]);
 });
 
-Route::get('', function () {
-    return view('');
-});
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/login', [UserController::class, 'login']);
 
+Route::post('/create-post', [PostController::class, 'createPost']);
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+Route::put('/edit-post/{post}', [PostController::class, 'actuallyUpdatePost']);
+Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
